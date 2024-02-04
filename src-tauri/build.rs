aa40c0521131586d35212f12d46c513f3caa9e1f
Changhua <lichanghua0821@gmail.com>
2024-02-04 19:48:51 +0800

    chore: add example for sending Text Message

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -6,9 +6,18 @@ fn main() {
             ".",
             "#[derive(serde::Serialize, serde::Deserialize, utoipa::ToSchema)]",
         )
+        .field_attribute(".wcf.WxMsg.type", "#[serde(rename = \"type\")]")
         .field_attribute(
-            ".wcf.WxMsg.type",
-            "#[serde(rename = \"type\")]",
+            ".wcf.TextMsg.msg",
+            "#[schema(example = \"换行用\n就可以了\n @昵称随便写\")]",
+        )
+        .field_attribute(
+            ".wcf.TextMsg.receiver",
+            "#[schema(example = \"88888888888@chatroom\")]",
+        )
+        .field_attribute(
+            ".wcf.TextMsg.aters",
+            "#[schema(example = \"wxid_88888888888888\")]",
         )
         .compile_protos(&["src/wcferry/lib/wcf.proto"], &["."])
         .unwrap();
