5885e5be03187c495ead81d222ed80fe4f3dd350
fantasy <timizqi@gmail.com>
2024-11-15 06:40:39 +0800

    feat: bump wcferry to v39.3.3 and upgrade roomdata proto

--- src-tauri/src/wcferry/roomdata.rs
+++ src-tauri/src/wcferry/roomdata.rs
@@ -4,16 +4,16 @@
 pub struct RoomData {
     #[prost(message, repeated, tag = "1")]
     pub members: ::prost::alloc::vec::Vec<room_data::RoomMember>,
-    #[prost(int32, tag = "2")]
-    pub field_2: i32,
+    #[prost(int32, optional, tag = "2")]
+    pub field_2: ::core::option::Option<i32>,
     #[prost(int32, tag = "3")]
     pub field_3: i32,
-    #[prost(int32, tag = "4")]
-    pub field_4: i32,
+    #[prost(int32, optional, tag = "4")]
+    pub field_4: ::core::option::Option<i32>,
     #[prost(int32, tag = "5")]
     pub room_capacity: i32,
-    #[prost(int32, tag = "6")]
-    pub field_6: i32,
+    #[prost(string, optional, tag = "6")]
+    pub field_6: ::core::option::Option<::prost::alloc::string::String>,
     #[prost(int64, tag = "7")]
     pub field_7: i64,
     #[prost(int64, tag = "8")]
@@ -27,8 +27,8 @@ pub mod room_data {
     pub struct RoomMember {
         #[prost(string, tag = "1")]
         pub wxid: ::prost::alloc::string::String,
-        #[prost(string, tag = "2")]
-        pub name: ::prost::alloc::string::String,
+        #[prost(string, optional, tag = "2")]
+        pub name: ::core::option::Option<::prost::alloc::string::String>,
         #[prost(int32, tag = "3")]
         pub state: i32,
     }
