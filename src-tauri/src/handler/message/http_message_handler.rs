57a12770e6271d02c20d6572a2e06c6604be4a08
fantasy <timizqi@gmail.com>
2024-11-18 21:44:30 +0800

    fix: 仅对r#type为1的纯文本消息做正则过滤，其他消息默认转发

--- src-tauri/src/handler/message/http_message_handler.rs
+++ src-tauri/src/handler/message/http_message_handler.rs
@@ -1,9 +1,12 @@
 use async_trait::async_trait;
 
-use crate::{handler::event_entity::{Event, EventHandler}, service::global_service::GLOBAL};
+use crate::{
+    handler::event_entity::{Event, EventHandler},
+    service::global_service::GLOBAL,
+};
 
-use serde_json::json;
 use regex::Regex;
+use serde_json::json;
 
 /// 配置 http 回调地址后，将调用设置的url，
 pub struct HttpMessageHandler {
@@ -23,17 +26,19 @@ impl EventHandler for HttpMessageHandler {
                 log::debug!("未配置回调地址，跳过处理");
                 return;
             }
-
-            if let Some(ref regex_str) = msg_filter_regexp {
-                let regex = Regex::new(&regex_str).unwrap();
-                if !regex.is_match(&msg.content) {
-                    log::debug!("消息被过滤，内容: {:?}", &msg.content);
-                    return;
+            // 仅对文本消息做过滤，其他消息也默认转发，如好友消息，红包消息，链接消息等
+            if msg.r#type == 1 {
+                if let Some(ref regex_str) = msg_filter_regexp {
+                    let regex = Regex::new(&regex_str).unwrap();
+                    if !regex.is_match(&msg.content) {
+                        log::debug!("消息被过滤，内容: {:?}", &msg.content);
+                        return;
+                    }
+                } else {
+                    log::debug!("未配置正则过滤，所有消息转发")
                 }
-            }else{
-                log::debug!("未配置正则过滤，所有消息转发")
             }
-             for url in cburl {
+            for url in cburl {
                 log::debug!("http服务 {} 回调地址为: {:?}", self.id, url.clone());
                 if !url.starts_with("http") {
                     log::error!("http 转发消息失败，回调地址不合法");
@@ -51,7 +56,7 @@ impl EventHandler for HttpMessageHandler {
                     Err(e) => {
                         log::error!("转发消息失败：{}", e);
                     }
-                } 
+                }
             }
         }
     }
